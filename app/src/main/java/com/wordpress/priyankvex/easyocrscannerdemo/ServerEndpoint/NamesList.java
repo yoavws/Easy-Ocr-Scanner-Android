c3558f4a12000fb52095c8656065c6fb4ecd785c
yoav.weiss <yoav.weiss@imperva.com>
2018-12-19 14:08:07 +0200

    add serverAPI change app Name

---
+++ app/src/main/java/com/wordpress/priyankvex/easyocrscannerdemo/ServerEndpoint/NamesList.java
@@ -0,0 +1,11 @@
+package com.wordpress.priyankvex.easyocrscannerdemo.ServerEndpoint;
+
+import java.util.HashMap;
+
+class NamesList{
+    private HashMap<String,String> names2Mails;
+
+    public NamesList(HashMap<String,String> list){
+        names2Mails=list;
+    }
+}
---
+++ app/src/main/java/com/wordpress/priyankvex/easyocrscannerdemo/ServerEndpoint/ServerAPI.java
@@ -0,0 +1,7 @@
+package com.wordpress.priyankvex.easyocrscannerdemo.ServerEndpoint;
+
+import com.google.gson.Gson;
+
+public interface ServerAPI {
+    Gson getListOfNames();
+}
